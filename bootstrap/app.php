7ffc9fe2a5947b30cce99fc1490ef623b8679271
thisgleammm <thisgleam@gmail.com>
2025-12-05 11:23:31 +0700

    feat: implement RBAC with spatie/laravel-permission

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -21,6 +21,10 @@ return Application::configure(basePath: dirname(__DIR__))
             HandleInertiaRequests::class,
             AddLinkHeadersForPreloadedAssets::class,
         ]);
+
+        $middleware->alias([
+            'authorize' => \App\Http\Middleware\Authorize::class,
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
